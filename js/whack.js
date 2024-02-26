615d894ee4f3ddf8763ef0662ae52de931861d20
sangjun1126 <jeongsangjun1234@gmail.com>
2024-02-26 10:28:25 +0900

    두더지 게임 초기 세팅 완료

---
+++ js/whack.js
@@ -0,0 +1,5 @@
+const $timer = document.querySelector('#timer');
+const $score = document.querySelector('#score');
+const $game = document.querySelector('#game');
+const $start = document.querySelector('#start');
+const $$cells = document.querySelectorAll('.cell');
